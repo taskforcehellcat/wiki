0f9b310f4ad55b310d342115dda5196d6dac8076
Venrix <imvenrix@gmail.com>
2021-07-27 02:24:11 +0200

    expanded website
    added loading screen
    added footer template
    added current page indicator
    added main tags

--- js/load_template.js
+++ js/load_template.js
@@ -1,16 +1,36 @@
-var bodyContent;
-$.get('body-template.html', function(data) {
-    bodyContent = data;
-    $('body').prepend(bodyContent);
+/* --- load template --- */
+
+
+
+var contentData;
+$.get('nav-template.html', function(data) {
+    contentData = data;
+    $('main').prepend(contentData);
 });
 
-var headContent;
+var footerData;
+$.get('footer-template.html', function(data) {
+    footerData = data;
+    $('body').append(footerData);
+});
+
+var headData;
 $.get('head-template.html', function(data) {
-    headContent = data;
-    $('head').prepend(headContent);
+    headData = data;
+    $('head').prepend(headData);
 });
 
-/*
-$('#loadingscreen').css('opacity', '0');
-$('#loadingscreen').delay(6000).remove;
-*/
+
+
+/* --- loading screen --- */
+
+$('body').prepend('<div id="loading"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;display:block;" width="120px" height="120px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><circle cx="50" cy="50" fill="none" stroke="red" stroke-width="5" r="30" stroke-dasharray="165 59"><animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform></circle></svg></div>');
+
+
+const wait = (delay = 0) =>
+    new Promise(resolve => setTimeout(resolve, delay));
+
+document.addEventListener('DOMContentLoaded', () =>
+    wait(500).then(() => {
+        $("#loading").css("display", "none");
+    }));
