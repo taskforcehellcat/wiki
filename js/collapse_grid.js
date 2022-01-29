b5af70586903e03ac4086efa610351f09d97aac2
Venrix <35926556+Venrix@users.noreply.github.com>
2022-01-29 05:18:56 +0100

    fixed a bunch of stuff
    too long ago, but i do remember fixing responsiveness ¯\_(ツ)_/¯

--- js/collapse_grid.js
+++ js/collapse_grid.js
@@ -1,3 +1,6 @@
+/* --- used to collapse rows in the mission log generator --- */
+/* --- kinda gave up at some point  ¯\_(ツ)_/¯ --- */
+
 /*
 $(".gen_category_title").on('click', function(event) {
     if ($(".gen_general").height = "fit-content") {
--- js/load_template.js
+++ js/load_template.js
@@ -1,6 +1,6 @@
-/* --- load template --- */
-
-
+/* --- old static site generator (deprecated) --- */
+/* --- why did i ever think this was a good idea */
+/* --- the fuck even is this --- */
 
 var contentData;
 $.get('nav-template.html', function(data) {
@@ -19,33 +19,3 @@ $.get('head-template.html', function(data) {
     headData = data;
     $('head').prepend(headData);
 });
-
-
-
-/* --- loading screen --- */
-document.body.insertAdjacentHTML('afterbegin', '<div id="loading"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;display:block;" width="120px" height="120px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><circle cx="50" cy="50" fill="none" stroke="red" stroke-width="5" r="30" stroke-dasharray="165 59"><animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform></circle></svg></div>');
-
-
-const wait = (delay = 0) =>
-    new Promise(resolve => setTimeout(resolve, delay));
-
-var domLoaded = false
-document.addEventListener('DOMContentLoaded', () =>
-    wait(500).then(() => {
-        document.querySelector("#loading").style.display = "none";
-        domLoaded = true;
-    })
-);
-
-
-if (domLoaded = true) {
-    wait(500).then(() => {
-        var count = 0;
-        document.querySelector("#nav-logo").addEventListener("click", function() {
-            count += 1;
-            console.log(count);
-            if (count == 5)
-                window.location = "mlg.html";
-        });
-    })
-}
