f030d56608646dcc3c508b73d81579d44f5415fa
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-02 06:27:04 +0100

    added placeholder homepage text

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,1 +1,11 @@
-<div id="title" style="text-align: center;">hier könnte ihre werbung stehen :)</div>
+<h1 class="text-centered">
+    Willkommen!
+</h1>
+
+<h2 class="text-centered">Etwas über uns:</h2>
+
+<br><br>
+
+<h3>Wir sind eine deutsche ArmA 3 Milsim Unit!</h3>
+
+<p class="text-justified text-boxed">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore vitae explicabo animi ex? Corporis culpa eligendi tenetur nemo amet consequatur maiores? Nulla voluptatum nihil, amet, laboriosam officia praesentium impedit corporis, molestias distinctio necessitatibus deserunt! Voluptas cum cumque magni, numquam maiores temporibus nostrum, quisquam voluptate odio eius error doloremque in obcaecati harum ipsa accusantium at delectus quae nulla odit officia. In?</p>
