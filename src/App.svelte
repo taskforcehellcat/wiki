0436ab99ef302718b502ff8fc26ececcd0c44622
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-06 19:23:40 +0100

    updated return url

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
 
 <header>
 	<div>
-		<a href="/"><span class="material-icons">arrow_back_ios_new</span>back to homepage</a>
+		<a href="https://taskforcehellcat.tk/"><span class="material-icons">arrow_back_ios_new</span>back to homepage</a>
 	</div>
 
 	<div id="searcharea">
@@ -19,7 +19,7 @@
 <main>
 	<!-- Left Column (Logo & Article Navigation) -->
 	<div id="left-bar">
-		<div id="wiki-logo"><img alt="Task Force Hellcat Wiki" src="./img/tfhc wiki logo clean.png"></div>
+		<div id="wiki-logo"><img alt="Task Force Hellcat Wiki" src="./images/tfhc wiki logo clean.png"></div>
 		<div id="article-nav">
 			<div>contents</div>
 			<Template_nav/>
