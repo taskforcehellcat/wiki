60ac3c951e7958ad43fe00ad41c3216197f8dfde
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-07 20:24:20 +0100

    renamed template to wiki

---
+++ src/Wiki.svelte
@@ -0,0 +1,217 @@
+
+
+<style>
+    #wiki-wrapper {
+    min-height: 120vh;
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+    grid-template-rows: 10fr 1fr;
+    }
+    #nav-list-bar {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+
+
+#return-button {
+    height: 2rem;
+    width: 100%;
+    justify-self: end;
+}
+
+#nav-logo {
+    font-size: 28pt;
+    font-weight: 300;
+    color: #fff;
+    font-family: 'purista-web', 'Segoe UI', sans-serif;
+}
+
+#nav-logo>span {
+    font-weight: 700;
+}
+
+input {
+    background-color: none;
+    background: none;
+    border: none;
+}
+
+#nav-search {
+    background-color: #273252;
+    width: 100%;
+    height: 5.5rem;
+    border-radius: .7rem;
+    -webkit-border-radius: .7rem;
+    -moz-border-radius: .7rem;
+    -ms-border-radius: .7rem;
+    -o-border-radius: .7rem;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 10px;
+}
+
+#nav-search {
+    padding: 5%;
+}
+
+#nav-search input {
+    width: 100%;
+    height: 24px;
+    color: #fff;
+    font-size: 14pt;
+    font-weight: 300;
+}
+
+.nav-list-title {
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    margin-bottom: -2rem;
+}
+
+#nav-search .material-icons {
+    font-size: 20pt;
+    color: #94A2CF;
+}
+
+input::placeholder {
+    color: #94A2CF;
+    font-weight: 300;
+}
+
+input:focus,
+input:focus {
+    outline: none;
+}
+
+#nav-list-wrapper {
+    display: flex;
+    gap: 15px;
+    height: fit-content;
+    box-sizing: unset;
+}
+
+#nav-list-bar {
+    width: 3px;
+    background-color: #2D3653;
+    height: 100%;
+    border-radius: .2rem;
+    -webkit-border-radius: .2rem;
+    -moz-border-radius: .2rem;
+    -ms-border-radius: .2rem;
+    -o-border-radius: .2rem;
+}
+
+#nav-list-bar-thumb {
+    width: 100%;
+    background-color: #94A2CF;
+}
+
+#nav-list {
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: .8rem 0 .8rem 0;
+}
+
+nav {
+    height: 100vh;
+    background-color: #101B3B;
+    display: flex;
+    color: #fff;
+    flex-direction: column;
+    padding: 10%;
+    gap: 3rem;
+    color: #94A2CF;
+    position: sticky;
+    z-index: 99;
+    top: 0;
+    overflow-y: scroll;
+}
+
+nav::-webkit-scrollbar {
+    display: none;
+}
+</style>
+
+<script>
+    import { Router, Link, Route } from "svelte-routing";
+	export let url = "";
+    
+	import { onMount } from 'svelte';
+
+
+    
+	onMount(async () => {
+
+
+        const thumb = document.getElementById("nav-list-bar-thumb");
+		const sections = document.querySelectorAll("section");
+		const navList = document.getElementById('nav-list');
+		var sectionsArr = [];
+		var x = 1;
+
+		/* Konvertiert "Sections" NodeList zu Array (falls man's mal brauch x) */
+		for(var i = sections.length; i--; sectionsArr.unshift(sections[i]));
+
+		/* Fügt h2 zu allen elementen der NodeList */
+		sectionsArr.forEach(element=> {
+			element.insertAdjacentHTML("afterBegin", "<h2>" + element.id + "</h2>");
+			navList.innerHTML += '<a href="#' + element.id + '">' + element.id + '</a>';
+		});
+		
+		/* setzt nav-list-bar-thumb auf genaue höhe von einem navi punkt */
+		thumb.style.height = 1 / sections.length * 100 + "%";
+
+        window.addEventListener('scroll', (event) => {
+            thumb.style.marginTop = window.scrollY / (sections.length * 10) + "rem";
+        })
+
+        var sectionHeight = 0;
+        sections.forEach(element => {
+            sectionHeight += parseInt(window.getComputedStyle(element).height);
+        })
+        
+        console.log(sectionHeight)
+
+        document.body.minHeight = sectionHeight;
+	});
+</script>
+
+
+<div id="wiki-wrapper">
+
+    <nav>
+        <div id="nav-logo">TFHC <span>Wiki</span></div>
+        <div id="nav-search">
+            <span class="material-icons">search</span>
+            <input type="text" name="search" placeholder="Wiki durchsuchen...">
+            
+        </div>
+        
+        <div class="nav-list-title">navigation</div>
+        <div id="nav-list-wrapper">
+            <div id="nav-list-bar">
+                <div id="nav-list-bar-thumb">
+                </div>
+            </div>
+        <div id="nav-list">
+        </div>
+    </div>
+        <div class="nav-list-title">wiki</div>
+        <a href="/" id="return-button">
+            Return
+        </a>
+    </nav>
+
+    <main>
+        <slot name="content"></slot>
+    </main>
+    <footer>
+    </footer>
+</div>
