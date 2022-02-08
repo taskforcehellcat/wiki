aea328eca1fb8ae4750cb15f07604dcec9c958d4
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-08 03:00:44 +0100

    Delete Template_nav.svelte

--- src/Template_nav.svelte
+++
@@ -1,8 +0,0 @@
-<a href="/">Allgemein</a>
-<a href="/">Rollenprofil</a>
-<a href="/">Ausrüstung</a>
-<a href="/">Aufgabenbereiche</a>
-<a href="/">Einsatzgebiete</a>
-<a href="/">Kompetenzen</a>
-<a href="/">Ausbildungsgehalt</a>
-<a href="/">Kanonenfuttergrad</a>
