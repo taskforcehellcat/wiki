385e39fc19e61ab9e8018c5dec0cab24a097cbaf
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-08 03:02:13 +0100

    added "streitkräfte" placeholder

---
+++ src/Wiki/Streitkraefte.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Wiki from "../Wiki.svelte";
+</script>
+
+<Wiki>
+    <svelte:fragment slot="content">
+        <h1>Streitkräfte</h1>
+
+        <section id="Allgemeines">
+            <p></p>
+        </section>
+    </svelte:fragment>
+</Wiki>
