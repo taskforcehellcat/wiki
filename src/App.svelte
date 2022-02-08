ff4213cc39edbfaf95d1f7293d22b3d6d482b480
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-08 03:00:19 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,73 +1,33 @@
 <script>
-	import { onMount } from 'svelte';
-	import Template from "./Template.svelte";
-	import Template_nav from "./Template_nav.svelte";
-	import Template_img from "./Template_img.svelte";
-import { element } from 'svelte/internal';
-
-	onMount(() => {
-
-		const thumb = document.getElementById("nav-list-bar-thumb");
-		const sections = document.querySelectorAll("section");
-		const navList = document.getElementById('nav-list');
-		var sectionsArr = [];
-		var x = 1;
-
-		/* Konvertiert "Sections" NodeList zu Array (falls man's mal brauch x) */
-		for(var i = sections.length; i--; sectionsArr.unshift(sections[i]));
-
-		/* Fügt h2 zu allen elementen der NodeList */
-		sectionsArr.forEach(element=> {
-			element.insertAdjacentHTML("afterBegin", "<h2>" + element.id + "</h2>");
-			navList.innerHTML += '<a href="#' + element.id + '">' + element.id + '</a>';
-		});
-		
-		/* setzt nav-list-bar-thumb auf genaue höhe von einem navi punkt */
-		thumb.style.height = 1 / sections.length * 100 + "%";
-		
-	});
+	import Grundlagen		from "./Wiki/Grundlagen.svelte";
+	import Fuehrungskraefte	from "./Wiki/Fuehrungskraefte.svelte";
+	import Streitkraefte	from "./Wiki/Streitkraefte.svelte";
+	import Logistiker		from "./Wiki/Logistiker.svelte";
+	import Panzertruppen	from "./Wiki/Panzertruppen.svelte";
+	import Sanitaeter 		from "./Wiki/Sanitaeter.svelte";
+	import Aufklaerer 		from "./Wiki/Aufklaerer.svelte";
+	import Fuhrpark			from "./Wiki/Fuhrpark.svelte";
+	
+	import Home from "./Home.svelte";
+	import { Router, Link, Route } from "svelte-routing";
+	export let url = "";
 </script>
 
+<!--
+<Wiki/>
+-->
 
-	<nav>
-		<div id="nav-logo">TFHC <span>Wiki</span></div>
-		<div id="nav-search">
-			<span class="material-icons">search</span>
-			<input type="text" name="search" placeholder="Wiki durchsuchen...">
-			
-		</div>
-		
-		
-		<div class="nav-list-title">navigation</div>
-		<div id="nav-list-wrapper">
-			<div id="nav-list-bar">
-				<div id="nav-list-bar-thumb">
-				</div>
-			</div>
-		<div id="nav-list">
-		</div>
-		</div>
-		<div class="nav-list-title">wiki</div>
-		<a id="return-button">
-			Return
-		</a>
-	</nav>
+<Router url="{url}">
+	<Route path="/"><Home/></Route>
+	<Route path="grundlagen"><Grundlagen/></Route>
+	<Route path="fuehrungskraefte"><Fuehrungskraefte/></Route>
+	<Route path="streitkraefte"><Streitkraefte/></Route>
+	<Route path="logistiker"><Logistiker/></Route>
+	<Route path="panzertruppen"><Panzertruppen/></Route>
+	<Route path="sanitaeter"><Sanitaeter/></Route>
+	<Route path="aufklaerer"><Aufklaerer/></Route>
+	<Route path="fuhrpark"><Fuhrpark/></Route>
 
-	<main>
-		<Template />
-	</main>
 	
 
-
-
-
-
-
-<footer>
-
-</footer>
-
-
-
-<style>
-</style>
+</Router>
