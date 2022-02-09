08f7ede18c6fcb7cbe337e8215be5e8c7e2d9d19
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-09 02:49:20 +0100

    Create Basislogistiker.svelte

---
+++ src/Wiki/Basislogistiker.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Wiki from "../Wiki.svelte";
+</script>
+
+<Wiki>
+  <svelte:fragment slot="content">
+    <h1>Basis-Logistiker</h1>
+
+    <section id="Allgemeines">
+      <p />
+    </section>
+  </svelte:fragment>
+</Wiki>
