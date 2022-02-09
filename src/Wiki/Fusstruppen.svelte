67bbcbd30a1c2e9a95102725ae59cbe1a74fd965
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-09 02:49:25 +0100

    Create Fusstruppen.svelte

---
+++ src/Wiki/Fusstruppen.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Wiki from "../Wiki.svelte";
+</script>
+
+<Wiki>
+  <svelte:fragment slot="content">
+    <h1>Fußtruppen</h1>
+
+    <section id="Allgemeines">
+      <p />
+    </section>
+  </svelte:fragment>
+</Wiki>
