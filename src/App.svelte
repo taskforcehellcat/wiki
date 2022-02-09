fcef0f50fdaabf8db8f89601358f95584d6762fd
Venrix <35926556+Venrix@users.noreply.github.com>
2022-02-09 02:48:46 +0100

    added route paths for all pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,18 @@
 <script>
   import Grundlagen from "./Wiki/Grundlagen.svelte";
-  import Fuehrungskraefte from "./Wiki/Fuehrungskraefte.svelte";
-  import Streitkraefte from "./Wiki/Streitkraefte.svelte";
-  import Logistiker from "./Wiki/Logistiker.svelte";
   import Panzertruppen from "./Wiki/Panzertruppen.svelte";
   import Sanitaeter from "./Wiki/Sanitaeter.svelte";
   import Aufklaerer from "./Wiki/Aufklaerer.svelte";
-  import Fuhrpark from "./Wiki/Fuhrpark.svelte";
+
+  import Basislogistiker from "./Wiki/Basislogistiker.svelte";
+  import Fahrzeuge from "./Wiki/Fahrzeuge.svelte";
+  import Fusstruppen from "./Wiki/Fusstruppen.svelte";
+  import Helikopter from "./Wiki/Helikopter.svelte";
+  import Hubschrauberpiloten from "./Wiki/Hubschrauberpiloten.svelte";
+  import Kampfpioniere from "./Wiki/Kampfpioniere.svelte";
+  import Truppfuehrer from "./Wiki/Truppfuehrer.svelte";
+  import Truppfunker from "./Wiki/Truppfunker.svelte";
+  import Uavs from "./Wiki/Uavs.svelte";
 
   import Home from "./Home.svelte";
   import { Router, Link, Route } from "svelte-routing";
@@ -26,6 +32,16 @@
   <Route path="panzertruppen"><Panzertruppen /></Route>
   <Route path="sanitaeter"><Sanitaeter /></Route>
   <Route path="aufklaerer"><Aufklaerer /></Route>
+
+  <Route path="basis-logistiker"><Basislogistiker /></Route>
+  <Route path="fahrzeuge"><Fahrzeuge /></Route>
+  <Route path="fusstruppen"><Fusstruppen /></Route>
+  <Route path="helikopter"><Helikopter /></Route>
+  <Route path="hubschrauberpiloten"><Hubschrauberpiloten /></Route>
+  <Route path="kampfpioniere"><Kampfpioniere /></Route>
+  <Route path="truppfuehrer"><Truppfuehrer /></Route>
+  <Route path="truppfunker"><Truppfunker /></Route>
+  <Route path="uavs"><Uavs /></Route>
 
   <Route path="fuhrpark"><Fuhrpark /></Route>
 </Router>
