f25162bf4b2acb317a6257ee3bb607c109804112
ptolemy <30769742+tiredandunproductive@users.noreply.github.com>
2022-02-14 12:50:31 +0100

    refactoring

--- src/Home.svelte
+++ src/Home.svelte
@@ -10,48 +10,33 @@
 
   let fetchedResults = []; // holds output of searchFor()
   let query = ''; // holds the query
-  let showResultsBox = false; // whether the search bar is currently in use
-
+  let showResults = false; // whether the search bar is currently in use
   let searchResults = []; // used to generate sections in search results
+  
   import { dropDown } from "../public/scripts/navigation/nav.js";
   
   onMount(async () => {
-    const resultBox = document.getElementById("home-nav-results");
-    const searchBar = document.getElementById("home-nav-search");
-    const navList = document.getElementById("nav-list");
-    const searchInput = searchBar.querySelector("input");
-    const searchWrapper = document.getElementById("search-wrapper");
-
     dropDown(); // expandables
-
-    function showResultsBox() {
-      // if search bar is empty,
-      if (searchInput.value.length == 0) {
-        // set searchwrapper dataset "empty" to "true"
-        searchWrapper.dataset.empty = "true";
-        // show navigation links
-        navList.style.display = "flex";
-        // if search bar is not empty,
-      } else {
-        // set searchwrapper dataset "empty" to "false"
-        searchWrapper.dataset.empty = "false";
-        // set hide navigation links
-        navList.style.display = "none";
-      }
-    }
-
-    showResultsBox();
-    searchInput.addEventListener("input", showResultsBox);
   });
 
   const handleQuery = (e) => {
     query = e.target.value;
 
+    let searchInUse = (query.length !== 0);
+
+    // do the styling
+    // hide results box if search bar empty
+    document.getElementById('search-wrapper').dataset.empty = !searchInUse;
+    
+    // show dropdown link menues if search bar empty
+    document.getElementById('nav-list').style.display = ['none', 'flex'][!searchInUse];
+
+
     if (query.length > 2) {
       fetchedResults = searchFor(query);
-      showResultsBox = true;
+      showResults = true;
     } else {
-      showResultsBox = false;
+      showResults = false;
     }
 
     if (fetchedResults.length !== 0) {
@@ -105,7 +90,7 @@
       <input type="text" name="search" placeholder="Wiki durchsuchen..." on:input={handleQuery} />
     </div>
     <div id="home-nav-results">
-      {#if showResultsBox}
+      {#if showResults}
         {#if searchResults.length !== 0}
           {#each searchResults as page}
             <p><span class="searchPageHits">{page.hits}</span> Treffer auf "<Link class="searchPageTitle" to={page.secResults[0].link}>{page.title}</Link>" gefunden:</p>
