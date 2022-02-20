61c4857fcfb429e2d368e881ba122a1e30e7d3a5
LeonTReal <leongamer27@gmail.com>
2022-02-20 02:46:41 +0100

    Update Kommandant.svelte

--- src/Wiki/Kommandant.svelte
+++ src/Wiki/Kommandant.svelte
@@ -8,19 +8,25 @@
     <h1>Kommandant (Panzertruppe)</h1>
 
     <section id="Rollenprofil">
-      <p>Der Kommandant hat die Befehlsgewalt und trägt die Verantwortung für sein Fahrzeug und die dazugehörige Besatzung. Seine Priorität ist der Erhalt der Gefechtsbereitschaft seines Fahrzeuges durch adäquate Führung.</p>
+      <p>Der Kommandant hat die Befehlsgewalt und trägt die Verantwortung für sein Fahrzeug und die dazugehörige Besatzung. Seine Priorität ist der Erhalt der Gefechtsbereitschaft seines Fahrzeuges durch adäquate Führung seiner Besatzung. In der Befehlskette untersteht er dem Zugführer, der Anweisungen an einzelne Kommandanten oder den Zug erteilen kann.</p>
     </section>
 
-    <section id="Aufgabenbereich">
-      <p>Der Kommandant nimmt unmittelbar Einfluss auf das Verhalten seines Fahrzeuges und seiner Besatzung.</p>
+    <section id="Aufgaben">
+      <p>Der Kommandant nimmt unmittelbar Einfluss auf das Verhalten seines Fahrzeuges und seiner Besatzung. Das Aufgabenprofil des Kommandanten kann zwischen Fahrzeugtypen variieren, lässt sich aber wie folgt zusammenfassen:</p>
 
       <ol>
         <li>Herstellung der Gefechtsbereitschaft</li>
         <li>Anweisungen zur Bewegung des Fahrzeuges an den Fahrer</li>
         <li>Anweisungen zur Bedienung der Waffensysteme an den Richtschützen</li>
-        <li>Funkverkehr innerhalb seines Zuges</li>
+        <li>Führen des Funkverkehrs mit dem Zugführer</li>
         <li>Aktives Aufklären und Wahrung von Situationsbewusstsein</li>
+        <li>Bestimmen der Munitionsart der Hauptkanone</li>
+        <li>Einsatzvermittlung an seine Besatzung</li>
       </ol>
+    </section>
+
+    <section id="Weiteres">
+      <p>Die Kommandanten der Panzertruppen müssen verschiedene Fertigkeiten beherrschen, um ihr Fahrzeug möglichst sicher und effizient ins Gefecht zu führen. Dazu gehören die Beherrschung und Einhaltung der Funketikette, Kenntnisse über das eigene Fahrzeug, die Beherrschung des NATO-Alphabets und Identifikation von Fahrzeugtypen, die Fähigkeit im Team zu arbeiten und ein gutes Situationsbewusstsein. Es ist von Bedeutung stets die Übersicht zu behalten und Gefahrensituationen einschätzen zu können.</p>
     </section>
   </svelte:fragment>
 </Wiki>
