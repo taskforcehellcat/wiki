aa595c5613e838340a5481dfc3277e9d090459ac
LeonTReal <leongamer27@gmail.com>
2022-02-20 01:42:39 +0100

    added "Kommandant" article

--- src/Wiki/Kommandant.svelte
+++ src/Wiki/Kommandant.svelte
@@ -7,8 +7,25 @@
     <article id="kommandant" />
     <h1>Kommandant (Panzertruppe)</h1>
 
-    <section id="Allgemeines">
-      <p />
+    <section id="Rollenprofil">
+      <p>Der Kommandant hat die Befehlsgewalt und trägt die Verantwortung für sein Fahrzeug und die dazugehörige Besatzung. Seine Priorität ist der Erhalt der Gefechtsbereitschaft seines Fahrzeuges durch adäquate Führung.</p>
+    </section>
+
+    <section id="Aufgabenbereich">
+      <p>Der Kommandant nimmt unmittelbar Einfluss auf das Verhalten seines Fahrzeuges durch eine Vielzahl an Kommandos.</p>
+
+      <section>
+        <h3>Herstellung der Gefechtsbereitschaft</h3>
+        <h3>Anweisungen an den Kraftfahrer</h3>
+        <h3>Wechsel der Hauptgeschützmunition</h3>
+        <h3>Erteilung des Feuers und des Feuerhaltes</h3>
+        <h3>Benutzung des Nebelwerfers</h3>
+        <h3>Feldaufklärung und Zielanweisungen für den Richtschützen</h3>
+        <h3>Festlegung von Feuerbereich und Hauptschussrichtung</h3>
+        <h3>Befehl des Schießhaltes</h3>
+        <h3>Informationswechsel mit dem Zugführer und anderen Kommandanten</h3>
+        <h3>Zielfestlegung für Richtschützen</h3>
+      </section>
     </section>
   </svelte:fragment>
 </Wiki>
