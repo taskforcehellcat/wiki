b5305c25df2783b7496202fd28137e3cc1667d70
LeonTReal <leongamer27@gmail.com>
2022-03-05 02:31:57 +0100

    Update Richtschuetze.svelte

--- src/Wiki/Richtschuetze.svelte
+++ src/Wiki/Richtschuetze.svelte
@@ -5,10 +5,23 @@
 <Wiki>
   <svelte:fragment slot="content">
     <article id="richtschuetze" />
-    <h1>Richtschütze (Panzertruppe)</h1>
+    <h1>Richtschütze (Panzertruppen)</h1>
 
-    <section id="Allgemeines">
-      <p />
+    <section id="Rollenprofil">
+      <p>Der Richtschütze bedient die Waffenanlage seines Fahrzeuges. Er untersteht dem auf seinem Fahrzeug aufgesessenem Kommandanten, der ihm im Verlauf eines Einsatzes eine Vielzahl an Anweisungen erteilen kann. Durch das Abfeuern der Waffensysteme nimmt er unmittelbar Einfluss auf das Gefechtsgeschehen.</p>
+    </section>
+
+    <section id="Aufgaben">
+      <p>Das Aufgabenprofil eines Richtschützen kann je nach Fahrzeugklasse und Bewaffnung stark variieren, lässt sich aber in ein paar Punkten zusammenfassen.</p>
+
+      <ol>
+        <li>Beobachtung eines zugewiesenen Feuerbereiches/der Hauptschussrichtung</li>
+        <li>Meldung feindlicher Kräfte entsprechend der REZ-Regel</li>
+        <li>Direkte Bekämpfung des Feindes durch geleitetes Feuern</li>
+        <li>Indirekte Bekämpfung des Feindes durch freies Feuern</li>
+        <li>Schussbeobachtung, Treffermeldung durchführen</li>
+        <li>Meldung über Status feindlicher Ziele</li>
+      </ol>
     </section>
   </svelte:fragment>
 </Wiki>
