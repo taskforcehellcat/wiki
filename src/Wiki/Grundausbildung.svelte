a5982af8641b6ea1b2c1a5c422dc9e6bda53b65f
LeonTReal <leongamer27@gmail.com>
2022-06-23 21:42:58 +0200

    added Befehlsausgabe

--- src/Wiki/Grundausbildung.svelte
+++ src/Wiki/Grundausbildung.svelte
@@ -1,24 +1,87 @@
 <script lang="ts">
   import Wiki from "../Wiki.svelte";
+  import { tooltip, exampleBox } from "../wikitools";
 </script>
 
 <Wiki>
   <svelte:fragment slot="content">
     <article id="grundausbildung" />
-    <h1>Grundausbildung</h1>
+    <h1>Befehlsausgabe</h1>
 
-    <section id="Rollenprofil">
-      <p />
+    <section id="Einleitung">
+      <p>Die Befehlsausgabe erfolgt nach der Gliederung <span use:tooltip data-tooltip="kurz: LADEF">Lage, Auftrag, Durchführung, Einsatzunterstützung und Führungsunterstützung</span>. Diese Gliederung wird auf dieser Seite erläutert, um von jedem Spieler gedeutet und interpretiert werden zu können. Oft erfolgt die Befehlsausgabe auch in der Form <span use:tooltip data-tooltip="kurz: LAD">Lage, Auftrag und Durchführung</span>, da die entfallenen Punkte nur für Führungspersonal von Bedeutung sind.</p>
+
     </section>
-    <section id="Aufgaben">
-      <p />
+
+    <section id="Befehlsschema">
+      <section>
+        <h2>1. Lage</h2>
+          <section>
+            <h3>1.1) Feindliche Lage</h3>
+              <p>In diesem Punkt wird über die Lage feindlicher Kräfte im Operationsraum informiert. Dabei werden die Position, die Art und die Absicht des Feindes genauer beschrieben.</p>
+              <br>
+              <br>
+              <div use:exampleBox><p>„<i>In der Stadt Kavala im Nordwesten von Altis ist ein feindlicher Infanteriezug in Stellung. Vermutete Absicht des Zuges ist die Verteidigung mit einem Halbzug im Nordosten und einem Halbzug im Südwesten der Stadt, um eine zentrale Hafenanlage der Insel zu verteidigen.</i>“</p></div>
+
+            <h3>1.2) Eigene Lage</h3>
+              <p>In diesem Punkt wird über die Lage eigener Kräfte informiert. Dabei wird auf die Position und Art der eigenen Kräfte eingegangen. Des Weiteren kann hier die Absicht des Führungspersonals mitgeteilt werden.</p>
+              <br>
+              <br>
+              <div use:exampleBox><p>„<i>Ein mechanisierter Infanteriezug und ein Infanteriehalbzug der Task Force Hellcat befinden sich aktuell im Verfügungsraum <span use:tooltip data-tooltip="Meter">5000</span> östlich von Kavala. Absicht des Zugführers ist der Angriff in <b>drei</b> Phasen. In <b>Phase 1</b> nimmt der erste mechanisierte Halbzug den Südwesten Kavalas, der zweite mechanisierte Halbzug den Nordosten Kavalas. In <b>Phase 2</b> verteidigt der erste mechanisierte Halbzug den Südwesten Kavalas, der zweite mechanisierte Halbzug verteidigt den Nordosten Kavalas. In <b>Phase 3</b> nimmt der Infanteriehalbzug das Zentrum Kavalas. Absicht des Angriffs ist der Zugang zu einer zentralen Hafenanlage der Insel Altis.</i>“</p></div>
+
+            <h3>1.3) Zivile Lage</h3>
+              <p>In diesem Punkt wird über die Lage von Zivilisten im Operationsraum informiert. Dabei wird auf das Verhalten der Zivilisten gegenüber dem Feind und eigenen Kräften eingegangen.</p>
+              <br>
+              <br>
+              <div use:exampleBox><p>„<i>Die Zivilbevölkerung im Raum Kavala geht dem Alltäglichen nach. Zivilisten meiden den Kontakt zu feindlichen Kräften und treten den eigenen Kräften neutral gegenüber.</i>“</p></div>
+          </section>
+
+        <h2>2. Auftrag</h2>
+          <p>In diesem Punkt erfolgt eine knappe Darlegung des <span use:tooltip data-tooltip="z.B. eines Zuges oder einer Gruppe">eigenen</span> Auftrags. Das geschieht in Form einer stichpunktartigen Aufzählung, was erledigt werden muss. Hauptsächlich wird dieser Punkt auf Zugebene besprochen.</p>
+          <br>
+          <br>
+          <div use:exampleBox><p>„<i>Der Infanteriehalbzug nimmt das Zentrum der Stadt Kavala.</i>“</p></div>
+
+        <h2>3. Durchführung</h2>
+          <p>In diesem Punkt werden die Einzelaufträge der <span use:tooltip data-tooltip="z.B. die Gruppen eines Zuges">unterstellten Truppenteile</span> erläutert. Dabei geht der Zug- bzw. Gruppenführer genauer auf die Vorgehensweise bei der Erfüllung des Auftrages ein. Zusätzlich können hier <span use:tooltip data-tooltip="z.B. Funkkreiseröffnung">wichtige Zeiten</span> oder <span use:tooltip data-tooltip="z.B. Einsatz von gelenkten Raketen nur auf Befehl">Verhaltensweisen</span> mitgeteilt werden.</p>
+          <br>
+          <br>
+          <div use:exampleBox><p>„<i>Die beiden Halbzüge des mechanisierten Infanteriezuges bewegen sich nebeneinander auf Kavala zu. Ab Ablauflinie Adler trennen sich die beiden Halbzüge und bewegen sich jeweils auf den ihnen zugewiesenen südwestlichen beziehungsweise nordöstlichen Teil Kavalas zu und nehmen diesen.</i>“</p></div>
+
+        <h2>4. Einsatzunterstützung</h2>
+          <section>
+            <h3>4.1) Logistische Unterstützung</h3>
+              <p>In diesem Punkt wird über Maßnahmen und Einrichtungen der <span use:tooltip data-tooltip="z.B. Munition und SanMat für Soldaten oder Instandhaltung der Fahrzeuge">Versorgung</span> informiert.</p>
+              <br>
+              <br>
+              <div use:exampleBox><p>„<i>Verbrauchsmaterial und Ersatzketten befinden sich auf den Schützenpanzern der mechanisierten Infanterie. Zur Versorgung des Infanteriehalbzuges steht ein Unimog mit Verbrauchsmaterial bereit.</i>“</p></div>
+
+            <h3>4.2) Sanitätsdienstliche Unterstützung</h3>
+              <p>In diesem Punkt wird über Möglichkeiten der Versorgung verwundeter Kräfte informiert.</p>
+              <br>
+              <br>
+              <div use:exampleBox><p>„<i>Vorrangig erfolgt die Versorgung verwundeter Soldaten im Feld durch Selbst- und Kameradenhilfe. Jeder Panzergrenadiertrupp und beide Gruppen des Infanteriehalbzuges verfügen jeweils über einen Sanitäter. Für medizinische Notfälle steht ein <span use:tooltip data-tooltip="kurz: BAT">beweglicher Arzttrupp</span> aufgesessen auf einem Unimog bereit.</i>“</p></div>
+          </section>
+
+        <h2>5. Führungsunterstützung</h2>
+          <section>
+            <h3>Funkverbindungsplan</h3>
+              <p>In diesem Punkt wird über die Zuweisung der Funkgeräte, der Funkfrequenzen und der Aufteilung der Funkrufzeichen informiert.</p>
+              <br>
+              <br>
+              <div use:exampleBox><p>„<i>Der Kompaniekreis befindet sich auf der SEM 70 mit der Frequenz 60.000 MHz. Im Zugkreis wird das Funkgerät AN/PRC-152 verwendet. Dabei ist für den mechanisierten Infanteriezug Kanal 01 und für den Infanteriehalbzug Kanal 02 reserviert. Auf Gruppenebene wird mit der SEM 52 SL gefunkt. Für die vier Gruppen des mechanisierten Infanteriezuges sind Kanäle 01 bis 04 reserviert. Der Infanteriehalbzug verteilt sich auf den Kanälen 05 und 06.</i>“</p>
+              <br>
+              <br>
+              <p>„<i>Der mechanisierte Infanteriezug ist in ALPHA und der Infanteriehalbzug in BRAVO eingeteilt.</i>“</p></div>
+
+            <h3>Führungsreihenfolge</h3>
+              <p>In diesem Punkt wird über die Einteilung der Führungsübernahme bei einem Ausfall der Führungsperson informiert.</p>
+
+              <div use:exampleBox><p>„<i>Die Kompanieführung beginnt beim Zugführer von ALPHA. Fällt dieser aus, übernimmt der Gruppenführer von ALPHA-2. Im Falle seines Ausfalls übernimmt der Zugführer von BRAVO die Kompanie.</i>“</p></div>
+          </section>
+      </section>
     </section>
-    <section id="Ausrüstung">
-      <p />
-    </section>
-    <section id="Fertigkeiten">
-      <p />
-    </section>
+
   </svelte:fragment>
-  <svelte:fragment slot="editdate">00.00.0000</svelte:fragment>
+  <svelte:fragment slot="editdate">23.06.2022</svelte:fragment>
 </Wiki>
