5445a69f65e51ae3995f0f749da00fe3d4c95467
zerofret <35926556+Venrix@users.noreply.github.com>
2022-06-23 02:28:56 +0200

    renamed "tooltip.js" to "wikitools.js"

--- src/wikitools.js
+++ src/wikitools.js
@@ -1,3 +1,67 @@
+export function tooltip(element) {
+  let div;
+  let tooltip;
+  let tooltipimg;
+
+  function mouseOver(event) {
+    // NOTE: remove the `tooltip` attribute, to prevent showing the default browser tooltip
+    // remember to set it back on `mouseleave`
+    tooltip = element.getAttribute("data-tooltip");
+    tooltipimg = element.getAttribute("data-tooltip-img");
+    //if element has attribute data-tooltip
+
+    if (tooltip) {
+      element.removeAttribute("data-tooltip");
+      div = document.createElement("div");
+      div.innerHTML = tooltip;
+      div.className = "tooltip";
+      div.style = `
+        top: ${event.pageX + 5}px;
+        left: ${event.pageY + 5}px;
+      `;
+      document.body.appendChild(div);
+    } else if (tooltipimg) {
+      div = document.createElement("div");
+      div.textContent = tooltip;
+      div.innerHTML = "<img style='max-width: 45rem;' src='" + tooltipimg + "'>";
+      div.className = "tooltip";
+      div.style = `
+        top: ${event.pageX + 5}px;
+        left: ${event.pageY + 5}px;
+      `;
+      document.body.appendChild(div);
+    }
+  }
+
+  function mouseMove(event) {
+    div.style.left = `${event.pageX + 5}px`;
+    div.style.top = `${event.pageY + 5}px`;
+  }
+
+  function mouseLeave() {
+    document.body.removeChild(div);
+    // NOTE: restore the `tooltip` attribute
+
+    if (tooltip) {
+      element.setAttribute("data-tooltip", tooltip);
+    } else if (tooltipimg) {
+      element.setAttribute("data-tooltip-img", tooltipimg);
+    }
+  }
+
+  element.addEventListener("mouseover", mouseOver);
+  element.addEventListener("mouseleave", mouseLeave);
+  element.addEventListener("mousemove", mouseMove);
+
+  return {
+    destroy() {
+      element.removeEventListener("mouseover", mouseOver);
+      element.removeEventListener("mouseleave", mouseLeave);
+      element.removeEventListener("mousemove", mouseMove);
+    },
+  };
+}
+
 export function exampleBox(element) {
   let div;
   let tooltip;
