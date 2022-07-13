f1cbd2619375c25901d3fad7223da92200309c4e
mreinhardt <mreinhardt@seibert-media.net>
2022-07-13 05:30:13 +0200

    remove stores.js, fix stores.ts imports

--- src/lib/theme/stores.js
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import { browser } from '$app/env';
-
-export const themeId = writable((browser && localStorage.getItem('USERTHEME')) || 'light');
--- src/lib/theme/stores.ts
+++ src/lib/theme/stores.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
-import { browser } from '$app/env';
+import {writable} from 'svelte/store';
+import {browser} from '$app/env';
 
 export const themeId = writable((browser && (localStorage.getItem('theme') || "auto")));
