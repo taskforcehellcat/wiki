a9650d1f12b095afbedfb3a020cf6969a39fb95e
Venrix <imvenrix@gmail.com>
2022-07-14 20:51:34 +0200

    Create pagetransition.svelte

---
+++ src/lib/pagetransition/pagetransition.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { fly } from "svelte/transition";
+	export let url = "";
+	const pageTransitionDuration = 500;
+  </script>
+  
+  {#key url}
+	<div in:fly={{  x:-5, duration: pageTransitionDuration, delay: pageTransitionDuration }}
+		 out:fly={{ x: 5, duration: pageTransitionDuration }}>
+	  <slot />
+	</div>
+  {/key}
