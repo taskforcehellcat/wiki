6f6b1ef43137baa12b30139e6c3c0f892ac90fe6
LeonTReal <leongamer27@gmail.com>
2022-09-21 21:12:32 +0200

    Update page "Schuetze"

--- src/routes/schuetze/index.svelte
+++ src/routes/schuetze/index.svelte
@@ -14,18 +14,44 @@
 
 		<section id="Rollenprofil">
 			<p>
-				Der Schütze ist die grundlegendste Rolle der Task Force Hellcat. Jedes neue Mitglied beginnt gleich nach der Grundausbildung mit der Rolle des Schützen, da ihre Hauptaufgabe die fachgerechte Anwendung der Grundkenntnisse ist. Als Rekrut kann man zusätzlich zur Rolle des Schützen eine Ausbildung auf eine Spezialisierung wie beispielsweise <span use:tooltip data-tooltip="AT = Anti Tank (Panzerabwehr)">AT</span>-Schütze oder Präzisionsschütze absolvieren.
+				Der Schütze ist die grundlegendste Rolle der Task Force Hellcat. Mit der Grundausbildung erhält jedes neue Mitglied den Zugang zur Rolle des Schützen. Die Kenntnisse und Fähigkeiten eines Schützen stellen die Grundlagen des Mitspielens dar, auf die alle anderen Rollen aufbauen.
 			</p>
 		</section>
 		<section id="Aufgaben">
-			<p>Die Rolle des Schützen ist das bekämpfen und niederhalten von feindlichen Kräften</p>
+			<p>Schützen nehmen unmittelbaren Einfluss auf die Gefechtseffektivität ihres Trupps beziehungsweise ihrer Gruppe. Das Aufgabenprofil der Schützen gestaltet sich dabei Vielfältig. Zu den Hauptaufgaben eines Schützen zählen die folgenden Punkte.</p>
+
+			<ol>
+				<li>Ausführen der Befehle des Trupp- oder Gruppenführers</li>
+				<li>Führen des Feuergefechts mit direktem oder indirektem Feuer mit Handfeuerwaffen</li>
+				<li>Leisten der Ersten Hilfe an Kameraden oder sich selbst</li>
+				<li>Fachgerechter Informationsaustausch über das Funkgerät auf Trupp- oder Gruppenebene</li>
+				<li>Bedienung von Orientierungsmitteln wie Karte und Kompass</li>
+				<li>Fahren von Fahrzeugen und Bedienung derer Waffenanlagen</li>
+				<li>Bewegen in taktischen Formationen im Feld</li>
+			</ol>
 		</section>
 		<section id="Ausrüstung">
-			<p />
+			<p>Die Ausrüstung des Schützen stellt die Grundausrüstung vieler anderer Rollen dar. Zusätzlich kann der Schütze besondere Ausrüstungsgegenstände anderer Rollen tragen, wie zum Beispiel Ersatzläufe eines MG-Schützen oder zusätzliche Munition. Zur Ausrüstung eines jeden Schützen zählen folgende Gegenstände:</p>
+
+			<ol>
+				<li>Eine Primärwaffe für das Gefecht auf mittlere Distanz</li>
+				<li>Eine Sekundärwaffe für das Gefecht auf kurze Distanz</li>
+				<li>Ein Funkgerät für den Informationsaustausch auf Trupp- oder Gruppenebene</li>
+				<li>Ein Spaten für das Errichten von Schützengräben im Feld</li>
+				<li>Eine Karte und einen Kompass zur Orientierung im Feld</li>
+				<li>Ein Fernglas zur Feldaufklärung</li>
+			</ol>
 		</section>
 		<section id="Fertigkeiten">
-			<p />
+			<p>Schützen müssen die grundlegendsten und damit wichtigsten Fertigkeiten beherrschen, auf die viele andere Rollen aufbauen. Dazu zählen folgende Punkte:</p>
+
+			<ol>
+				<li>Kenntnisse über Formationen und Bewegung im Feld</li>
+				<li>Beherrschung der <a href="../erstehilfe">Ersten Hilfe</a> zur Selbst- und Fremdversorgung</li>
+				<li>Beherrschung der <a href="../funketikette">Funketikette</a></li>
+				<li>Kenntnisse zur <a href="../steuerung">Bedienung von Handfeuerwaffen</a></li>
+			</ol>
 		</section>
 	</svelte:fragment>
-	<svelte:fragment slot="editdate">22.06.2022</svelte:fragment>
+	<svelte:fragment slot="editdate">21.09.2022</svelte:fragment>
 </Wiki>
