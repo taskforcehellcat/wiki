01407951573c130221a2eb0c0581efd41ada7554
emmy <30769742+tiredandunproductive@users.noreply.github.com>
2022-09-23 18:14:25 +0200

    typos

--- src/routes/kampfpionier/index.svelte
+++ src/routes/kampfpionier/index.svelte
@@ -12,26 +12,26 @@
 		<article id="kampfpionier" />
 		<h1>Kampfpionier</h1>
 		<section id="Rollenprofil">
-			<p>Die Rolle des Kampfpioniers setzt sich zusammen aus der Rolle des Pioniers & des Schützen. Der Kampfpionier trägt die Verantwortung, den Schutz der eigenen Truppen zu verbessern und dadurch deren Überlebensfähigkeit zu erhöhen, ist anders als der Pionier aber aktiv am Kampfgefecht beteiligt.</p>
+			<p>Die Rolle des Kampfpioniers setzt sich zusammen aus der Rolle des Pioniers und des Schützen. Der Kampfpionier trägt die Verantwortung, den Schutz der eigenen Truppen zu verbessern und dadurch deren Überlebensfähigkeit zu erhöhen, ist anders als der Pionier aber aktiv am Kampfgefecht beteiligt.</p>
 		</section>
 		<section id="Aufgaben">
-			<p>Die Aufgaben eines Kampfpioniers variieren je nach Mission & Situation. Die Hauptaufgaben lassen sich aber in wenigen Punkten grob zusammenfassen.</p>
+			<p>Die Aufgaben eines Kampfpioniers variieren je nach Mission und Situation. Die Hauptaufgaben lassen sich aber in wenigen Punkten grob zusammenfassen.</p>
 			<ul>
-				<li>Die Instandsetzung von Boden- und Luftfahrzeugen</li>
-				<li>Die Instandsetzung von Infrastruktur, als auch das verstärken und befestigen militärisch genutzter Einrichtungen durch bauliche Maßnahmen</li>
-				<li>Das befördern von außerstand-gesetzten Fahrzeugen</li>
+				<li>die Instandsetzung von Boden- und Luftfahrzeugen</li>
+				<li>die Instandsetzung von Infrastruktur, als auch das verstärken und befestigen militärisch genutzter Einrichtungen durch bauliche Maßnahmen</li>
+				<li>das Befördern von außerstand-gesetzten Fahrzeugen</li>
 				<li>
-					Das Aufklären und entschärfen von sowohl <span use:tooltip data-tooltip="Anti-personnel (Antipersonen)">AP</span>- & <span use:tooltip data-tooltip="Anti-tank (Panzerabwehr)">AT</span>-Minen
+					das Aufklären und entschärfen von sowohl <span use:tooltip data-tooltip="Anti-personnel (Antipersonen)">AP</span>- & <span use:tooltip data-tooltip="Anti-tank (Panzerabwehr)">AT</span>-Minen
 				</li>
 				<li>
-					Das bedienen von <span use:tooltip data-tooltip="Unmanned ground vehicle (unbemanntes Bodenfahrzeug)">UGV</span>
-					&
+					das Bedienen von <span use:tooltip data-tooltip="Unmanned ground vehicle (unbemanntes Bodenfahrzeug)">UGV</span>
+					und
 					<span use:tooltip data-tooltip="Unmanned aerial vehicle (unbemanntes Fluggerät)">UAV</span> Dronen
 				</li>
 			</ul>
 		</section>
 		<section id="Ausrüstung">
-			<p>In Ergänzung zu der Grundausrüstung des Schützen, zählen folgende Gegenstände zur Ausrüstung des Kampfpioniers:</p>
+			<p>In Ergänzung zu der Grundausrüstung des Schützen zählen folgende Gegenstände zur Ausrüstung des Kampfpioniers:</p>
 			<ul>
 				<li><span use:tooltip data-tooltip="Minensuchgerät">Mine Detector</span></li>
 				<li><span use:tooltip data-tooltip="Werkzeugkasten">Toolkit</span></li>
@@ -39,20 +39,20 @@
 					<span use:tooltip data-tooltip="Terminal zum Steuern von oben erläuterten UAVs & UGVs">UAV-Terminal</span>
 				</li>
 				<li>
-					<span use:tooltip data-tooltip="Klappspaten, zum aufschaufeln von Deckung im Feld">Entrenching Tool</span>
+					<span use:tooltip data-tooltip="Klappspaten, zum aufschaufeln von Deckung im Feld">Entrenching-Tool</span>
 				</li>
-				<li>Je nach Situation ein Minenräumungsfahrzeug, oder diverse Dronen</li>
+				<li>je nach Situation ein Minenräumungsfahrzeug, oder diverse Dronen</li>
 			</ul>
 		</section>
 		<section id="Fertigkeiten">
 			<ul>
-				<li>Erweiterte Kenntnisse über eigene Fahrzeuge</li>
-				<li>Bedienung der oben genannten Ausrüstung</li>
+				<li>erweiterte Kenntnisse über eigene Fahrzeuge</li>
+				<li>Verwendung der oben genannten Ausrüstung</li>
 			</ul>
 		</section>
 		<section id="Weiteres">
 			<p>Kampfpionier immer halal</p>
 		</section>
 	</svelte:fragment>
-	<svelte:fragment slot="editdate">00.00.0000</svelte:fragment>
+	<svelte:fragment slot="editdate">23.09.2022</svelte:fragment>
 </Wiki>
