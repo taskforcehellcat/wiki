83daf2a61c1fdb3c6b84711851f57c65ad0709fe
ptolemy <30769742+tiredandunproductive@users.noreply.github.com>
2022-09-23 04:46:39 +0200

    fix build process

--- src/lib/search/searchIndexUpdater.py
+++ src/lib/search/searchIndexUpdater.py
@@ -9,10 +9,7 @@ from json import dump
 from bs4 import BeautifulSoup
 
 # directory that contains the directories with page content
-deploying = False
 ROOT_PAGES = r'../../../src/routes/'
-if deploying:
-    ROOT_PAGES = r'/opt/build/repo/src/routes/'
 
 # symbol that's used to indicate that a section contains a subsection,
 # see also: http://xahlee.info/comp/unicode_arrows.html
