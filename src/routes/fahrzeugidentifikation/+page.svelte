6ae29ed755bc50e7836413fcfba89c8c2eff8e9c
LeonTReal <leongamer27@gmail.com>
2023-01-13 18:14:49 +0100

    rewrite and finalize Fahrzeugidentifikation

--- src/routes/fahrzeugidentifikation/+page.svelte
+++ src/routes/fahrzeugidentifikation/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import CloseMenu from '$lib/burgermenu/closeMenu.svelte';
 	import Wiki from '../wiki/+page.svelte';
+	import { tooltip } from '$lib/wikitools/wikitools';
 
 	/* Images */
 	import guide from '$lib/images/fahrzeuge/guide.png';
@@ -20,22 +21,32 @@
 		</section>
 
 		<section id="Kampfpanzer">
-			<p>Ein MBT (<b>M</b>ain <b>B</b>attle <b>T</b>ank, Kampfpanzer) ist ein schwer gepanzertes Kettenfahrzeug.</p>
-			<p>Er besteht grob zusammengefasst aus zwei Komponenten: eine Wanne und ein Turm. Das wohl auffälligste Erkennungsmerkmal ist der Turm mit der Hauptbewaffnung. Der Turm ist ungefähr in der Mitte der länglichen Wanne aufgebracht und kann diesen um 360° drehen. An diesem Turm ist ein langes Kanonenrohr angebracht was einen Durchmesser von über 100-mm hat. Der Turm selber ist hierbei meist relativ flach gehalten und nicht zu verwechseln mit den sehr großen Türmen einer Panzerhaubitze, die dazu auch ein größeres Kanonenrohr mit einem hohen positiven Richtwinkel besitzen.</p>
+			<p>Ein <span use:tooltip data-tooltip="kurz: KPz">Kampfpanzer</span> (engl. <span use:tooltip data-tooltip="kurz: MBT">Main Battle Tank</span>) ist ein schwer gepanzertes Kettenfahrzeug.</p>
+
+			<p>Ein moderner Kampfpanzer besteht grob zusammengefasst aus drei Komponenten: Einer schwer gepanzerten Wanne, einen (rundum drehbaren und meist mittig auf der Wanne platzierten) Turm und einer im Turm verbauten <span use:tooltip data-tooltip="meist mit einem Durchmesser von 120mm">Hauptkanone</span>. Kampfpanzer agieren in Panzerverbänden und niemals alleine, dabei werden sie oftmals von der Infanterie in Schützenpanzern begleitet. Kampfpanzer sind nicht darauf ausgelegt Infanteristen zu transportieren. Die Aufgabe von Kampfpanzern ist die Bekämpfung gegnerischer Panzerverbände und befestigter Stellungen.</p>
 		</section>
 
-		<section id="Flugabwehr">
-			<p>AA steht für „<b>A</b>nti <b>A</b>ir“ (Flugabwehr, umgangssprachlich auch <i>Flak</i>). Es ist ein System, dessen ursprünglicher Zweck die Zerstörung von feindlichen Lufteinheiten ist. Es gibt sie stationär und als Flugabwehrkanonenpanzer. Letztere sind, wie der Name schon sagt, auf Ketten unterwegs. Sie basieren meist auf der Wanne eines Kampfpanzers, haben aber einen deutlich anderen Turm. Auch dieser Turm ist speziell durch seine Bewaffnung gekennzeichnet. Es befinden sich meist mehrere großkalibrige Maschinenkanonen am Turm, welche einen positiven Richtwinkel von bis zu 90° haben können. Auch ihr Turm ist um 360° drehbar und hat meist auch das Radar aufsitzen. Das Radar ist, neben der Bewaffnung, wohl das markanteste Merkmal.</p>
+		<section id="Schützenpanzer">
+			<p>Ein <span use:tooltip data-tooltip="kurz: SPz">Schützenpanzer</span> (engl. <span use:tooltip data-tooltip="kurz: IFV">Infantry Fighting Vehicle</span>) ist ein Panzerfahrzeug, das sowohl auf Ketten als auch auf Rädern vorkommen kann.</p>
+
+			<p>Ein moderner Schützenpanzer agiert gemeinsam mit auf- und abgesessenen <span use:tooltip data-tooltip="Infanterie">Panzergrenadieren</span>. Grundsätzlich setzen sich Schützenpanzer aus einer gepanzerten Wanne, einem Geschützturm mit einer <span use:tooltip data-tooltip="meist mit einem Durchmesser von 20-30mm">automatischen Kanone</span> und einem Transportraum für die Panzergrenadiere zusammen. Oftmals werden sie gemeinsam mit Kampfpanzern in Panzerverbänden eingesetzt.</p>
+		</section>
+
+		<section id="Transportpanzer">
+			<p>Ein <span use:tooltip data-tooltip="kurz: TPz">Transportpanzer</span> (engl. <span use:tooltip data-tooltip="kurz: APC">Armored Personnel Carrier</span>) ist ein gepanzertes Fahrzeug, das zum Transport von Infanteristen eingesetzt wird.</p>
+
+			<p>Moderne Transportpanzer können in vielen Formen auftreten, unter anderem ähnlich wie Schützenpanzer mit Ketten oder Rädern. Hauptmerkmale sind ein großer Transportraum für Infanteristen und eine leichte Bordbewaffnung. Grundsätzlich sind Transportpanzer leichter gepanzert und leichter bewaffnet als Schützenpanzer. Dementsprechend werden sie auch nicht für den direkten Feuerkampf eingesetzt. Meist verfügen Transportpanzer über Maschinengewehre oder Granatwerfer, um einen sicheren Transport der Infanterie zu gewährleisten.</p>
 		</section>
 
 		<section id="Artilleriepanzer">
-			<p>Der Artilleriepanzer (<b>S</b>elf <b>P</b>ropelled <b>G</b>un, SPG) ist ein mobiles Artilleriegeschütz. Es besitzt ebenfalls eine Wanne und einen Turm. Beides ist meist größer als bei einem MBT. Auffällig ist das besonders große Geschütz mit einem Kaliber von 155-mm bis teilweise 203-mm. Sie haben einen hohen positiven Richtwinkel um auf große Entfernung wirken zu können. Einige Exemplare sind außerdem in der Lage ihren Turm samt Geschütz zu drehen.</p>
+			<p>Der Artilleriepanzer (engl. <span use:tooltip data-tooltip="kurz: SPG">Self-Propelled Gun</span>) ist ein mobiles Artilleriegeschütz. Hauptmerkmale sind eine große, leichtgepanzerte Wanne und ein großer (meist hinten an der Wanne platzierter) Turm mit großer <span use:tooltip data-tooltip="meist mit einem Durchmesser von 155mm">Kanone</span>. Artilleriepanzer haben einen großen positiven Richtwinkel der Kanone, um auf große Entfernungen wirken zu können. Ihre Aufgabe ist die Feuerunterstützung von verbündeten Kräften an der Front durch Steilfeuer, dabei oftmals ohne direkte Sicht auf das Ziel.</p>
 		</section>
 
-		<section id="Mannschaftstransporter">
-			<p>Der Schützenpanzer (<b>I</b>nfantry <b>F</b>ighting <b>V</b>ehicle, IFV) und der Mannschaftstransportwagen (<b>A</b>rmored <b>P</b>ersonnel <b>C</b>arrier, APC) sind äußerlich nicht allzu leicht zu unterscheiden. Sie sind beide ein fester Bestandteil ihres Trupps. Allerdings ist der APC oft auf Rädern unterwegs und sind gar nicht bis leicht, meist 7.62-mm, 50 cal, 14.5-mm oder 30/40-mm Granatwerfer, bewaffnet welche gegebenenfalls auch auf einen kleinen Turm montiert sein können.</p>
-			<p>Das IFV hingegen ist auf Ketten unterwegs und hat einen kleinen Turm welcher mit einem 20-mm-Geschütz oder noch größerem Kaliber sowie Panzerabwehrraketen bewaffnet ist.</p>
+		<section id="Flugabwehr">
+			<p>Der Sammelbegriff Flugabwehr umfasst viele verschiedene Fahrzeuge und Fahrzeugtypen. Wir konzentrieren uns vorrangig auf <span use:tooltip data-tooltip="kurz: FlakPz">Flakpanzer</span>. Diese Fahrzeuge werden zur Sicherung des Luftraumes auf kurze und mittlere Distanz verwendet, unter anderem oft mit mehreren <span use:tooltip data-tooltip="meist mit einem Durchmesser von 20 bis 35mm">Flugabwehrkanonen</span> und/oder Boden-zu-Luft-Raketen. Grob zusammengefasst verfügen die meisten Flakpanzer über eine leichtgepanzerte Wanne und einen Turm, an dem die Flugabwehrkanonen und/oder Raketen angebracht sind. Ein am Turm befestigtes <span use:tooltip data-tooltip="muss nicht immer vorhanden sein">Radar</span> sowie die Bewaffnung eines Flakpanzers sind wohl seine markantesten Merkmale.</p>
 		</section>
+
+		
 	</svelte:fragment>
-	<svelte:fragment slot="editdate">13.10.2022</svelte:fragment>
+	<svelte:fragment slot="editdate">13.01.2023</svelte:fragment>
 </Wiki>
