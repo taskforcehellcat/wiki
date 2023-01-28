2e5ea705db83f7a6c3d43623614e32826fa26ea7
Marcel Reinhardt <mreinhardt@seibert-media.net>
2023-01-28 15:03:42 +0100

    update some content

---
+++ src/content/ausbildung/fahrzeugidentifikation.svelte
@@ -0,0 +1,42 @@
+---
+title: Fahrzeugidentifikation
+date: "2022-11-15"
+nav_index: 1
+---
+
+
+Übersicht
+---------
+
+![Fahrzeugguide](https://wiki.taskforcehellcat.de/_app/immutable/assets/guide-2561491a.png)
+
+Kampfpanzer
+-----------
+
+Ein Kampfpanzer (engl. Main Battle Tank) ist ein schwer gepanzertes Kettenfahrzeug.
+
+Ein moderner Kampfpanzer besteht grob zusammengefasst aus drei Komponenten: Einer schwer gepanzerten Wanne, einen (rundum drehbaren und meist mittig auf der Wanne platzierten) Turm und einer im Turm verbauten Hauptkanone. Kampfpanzer agieren in Panzerverbänden und niemals alleine, dabei werden sie oftmals von der Infanterie in Schützenpanzern begleitet. Kampfpanzer sind nicht darauf ausgelegt Infanteristen zu transportieren. Die Aufgabe von Kampfpanzern ist die Bekämpfung gegnerischer Panzerverbände und befestigter Stellungen.
+
+Schützenpanzer
+--------------
+
+Ein Schützenpanzer (engl. Infantry Fighting Vehicle) ist ein Panzerfahrzeug, das sowohl auf Ketten als auch auf Rädern vorkommen kann.
+
+Ein moderner Schützenpanzer agiert gemeinsam mit auf- und abgesessenen Panzergrenadieren. Grundsätzlich setzen sich Schützenpanzer aus einer gepanzerten Wanne, einem Geschützturm mit einer automatischen Kanone und einem Transportraum für die Panzergrenadiere zusammen. Oftmals werden sie gemeinsam mit Kampfpanzern in Panzerverbänden eingesetzt.
+
+Transportpanzer
+---------------
+
+Ein Transportpanzer (engl. Armored Personnel Carrier) ist ein gepanzertes Fahrzeug, das zum Transport von Infanteristen eingesetzt wird.
+
+Moderne Transportpanzer können in vielen Formen auftreten, unter anderem ähnlich wie Schützenpanzer mit Ketten oder Rädern. Hauptmerkmale sind ein großer Transportraum für Infanteristen und eine leichte Bordbewaffnung. Grundsätzlich sind Transportpanzer leichter gepanzert und leichter bewaffnet als Schützenpanzer. Dementsprechend werden sie auch nicht für den direkten Feuerkampf eingesetzt. Meist verfügen Transportpanzer über Maschinengewehre oder Granatwerfer, um einen sicheren Transport der Infanterie zu gewährleisten.
+
+Artilleriepanzer
+----------------
+
+Der Artilleriepanzer (engl. Self-Propelled Gun) ist ein mobiles Artilleriegeschütz. Hauptmerkmale sind eine große, leichtgepanzerte Wanne und ein großer (meist hinten an der Wanne platzierter) Turm mit großer Kanone. Artilleriepanzer haben einen großen positiven Richtwinkel der Kanone, um auf große Entfernungen wirken zu können. Ihre Aufgabe ist die Feuerunterstützung von verbündeten Kräften an der Front durch Steilfeuer, dabei oftmals ohne direkte Sicht auf das Ziel.
+
+Flugabwehr
+----------
+
+Der Sammelbegriff Flugabwehr umfasst viele verschiedene Fahrzeuge und Fahrzeugtypen. Wir konzentrieren uns vorrangig auf Flakpanzer. Diese Fahrzeuge werden zur Sicherung des Luftraumes auf kurze und mittlere Distanz verwendet, unter anderem oft mit mehreren Flugabwehrkanonen und/oder Boden-zu-Luft-Raketen. Grob zusammengefasst verfügen die meisten Flakpanzer über eine leichtgepanzerte Wanne und einen Turm, an dem die Flugabwehrkanonen und/oder Raketen angebracht sind. Ein am Turm befestigtes Radar sowie die Bewaffnung eines Flakpanzers sind wohl seine markantesten Merkmale.
