f7747ae2d8297840a951bc54faab339e4356aa7d
Emmi <30769742+pantoffelemmy@users.noreply.github.com>
2023-02-27 12:51:48 +0100

    different approach using svelte-meta-tags

--- src/routes/articles/[dirslug]/[articleslug]/+page.svelte
+++ src/routes/articles/[dirslug]/[articleslug]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { afterNavigate } from '$app/navigation';
-  import OpenGraph from '$lib/seo/OpenGraph.svelte';
+  import { MetaTags } from 'svelte-meta-tags';
   export let data;
 
   const umlautReplacements = [
@@ -29,15 +29,7 @@
   // See https://github.com/sveltejs/kit/issues/627
   // import Layout from '../+layout.svelte'
   // Or: Fix footer another way
-
-  const propsOpenGraph = {
-    imgUrl: `https://feature-opengraph.wiki.taskforcehellcat.de/images/thumbnails/${data.articleslug}.png`,
-    url: `http://feature-opengraph.wiki.taskforcehellcat.de/articles/${data.dirslug}/${data.articleslug}.png`,
-    title: `${data.title} · TFHC Wiki`
-  };
 </script>
-
-<OpenGraph {...propsOpenGraph} />
 
 <section>
   <h1>{data.title}</h1>
@@ -45,3 +37,30 @@
   <!-- Use svelte:component instead of <Content/> to force reactivity if content changes (like changing articles) -->
   <svelte:component this={data.Content} />
 </section>
+
+<MetaTags
+  title={data.title}
+  titleTemplate="%s · TFHC Wiki"
+  canonical="https://wiki.taskforcehellcat.de/"
+  openGraph={{
+    url: `https://www.url.ie/articles/${data.dirslug}/${data.articleslug}`,
+    title: `${data.title}`,
+    description: 'Platzhalter',
+    images: [
+      {
+        url: `https://feature-opengraph.wiki.taskforcehellcat.de/images/thumbnails/${data.articleslug}.png`,
+        width: 1200,
+        height: 627,
+        alt: 'Artikelbanner'
+      }
+    ],
+    site_name: 'Task Force Hellcat Wiki'
+  }}
+  twitter={{
+    cardType: 'summary_large_image',
+    title: `${data.title}`,
+    description: 'Platzhalterbeschreibung',
+    image: `https://feature-opengraph.wiki.taskforcehellcat.de/images/thumbnails/${data.articleslug}.png`,
+    imageAlt: 'Artikelbanner'
+  }}
+/>
