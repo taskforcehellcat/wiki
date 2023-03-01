523839b4ba2b2e3eb00c14f378605077290b7107
unknown <30769742+pantoffelemmy@users.noreply.github.com>
2023-03-01 04:38:10 +0100

    add component

---
+++ src/lib/linkpreview/PageMeta.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+</svelte:head>
