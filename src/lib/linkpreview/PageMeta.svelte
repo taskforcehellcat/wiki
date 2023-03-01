46c9d0f1e308b0696c0eb170d1e2d74df2e313ff
Emmi <30769742+pantoffelemmy@users.noreply.github.com>
2023-03-01 05:02:56 +0100

    ssr testing 3

--- src/lib/linkpreview/PageMeta.svelte
+++ src/lib/linkpreview/PageMeta.svelte
@@ -18,7 +18,7 @@
   />
   <meta property="og:type" content="website" />
   <meta property="og:title" content={title} />
-  <meta property="og:description" content="Platzhalter" />
+  <meta property="og:description" content="Artikel im TFHC Wiki" />
   <meta
     property="og:image"
     content="https://feature-opengraph.wiki.taskforcehellcat.de/images/thumbnails/{article}.png"
@@ -32,7 +32,7 @@
     content="https://feature-opengraph.wiki.taskforcehellcat.de/articles/ausbildung/fahrzeugerkennung"
   />
   <meta name="twitter:title" content={title} />
-  <meta name="twitter:description" content="bla" />
+  <meta name="twitter:description" content="Artikel im TFHC Wiki" />
   <meta
     name="twitter:image"
     content="https://feature-opengraph.wiki.taskforcehellcat.de/images/thumbnails/{article}.png"
