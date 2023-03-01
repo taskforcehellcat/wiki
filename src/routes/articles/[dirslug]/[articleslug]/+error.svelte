47c63246e8453c0ae658989d4b304dddaba565e0
unknown <30769742+pantoffelemmy@users.noreply.github.com>
2023-03-01 13:35:05 +0100

    catch errors in the layout

---
+++ src/routes/articles/[dirslug]/[articleslug]/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<h1>Fehler {$page.status}: {$page.error.message}</h1>
+
+{#if $page.status == 404}
+  <p>Diese Seite konnten wir nicht finden.</p>
+  <p>Wenn du glaubst, sie sollte existieren, lass es uns wissen: <a href="https://discord.com/channels/629333468299526164/1021556229857882182" target="_blank" rel="noreferrer">Feedback-Forum auf Discord</a></p>
+{/if}
+
+{#if $page.status == 500}
+  <p>Irgendwas ist bei uns schief gelaufen, das hätte nicht passieren sollen.</p>
+  <p>Sag uns gerne, wie du hier hier gelandet bist, damit wir das Problem beheben können: <a href="https://discord.com/channels/629333468299526164/1021556229857882182" target="_blank" rel="noreferrer">Feedback-Forum auf Discord</a></p>
+{/if}
