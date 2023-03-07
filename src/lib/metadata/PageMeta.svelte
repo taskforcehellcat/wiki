2905ec5a97a2741e4b93463f8c76afcbad397a7b
Emmi <30769742+pantoffelemmy@users.noreply.github.com>
2023-03-07 15:53:25 +0100

    temporary change in the thumbnails serving url, development only

--- src/lib/metadata/PageMeta.svelte
+++ src/lib/metadata/PageMeta.svelte
@@ -7,6 +7,10 @@
   let description = data.description ?? 'Artikel im TFHC Wiki';
 
   let url = `https://wiki.taskforcehellcat.de/articles/${dir}/${article}`;
+
+  // FIXME use this in production:
+  //let thumbnailsUrl = `https://wiki.taskforcehellcat.de/images/thumbnails/`;
+  let thumbnailsUrl = `https://feature-opengraph.wiki.taskforcehellcat.de/images/thumbnails/`
 </script>
 
 <svelte:head>
@@ -27,5 +31,5 @@
   <meta property="twitter:url" content={url} />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content="https://wiki.taskforcehellcat.de/images/thumbnails/{article}.png" />
+  <meta name="twitter:image" content={`${thumbnailsUrl}${article}.png`} />
 </svelte:head>
