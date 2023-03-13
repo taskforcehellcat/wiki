8328d9b6f32890d7b5ce4e2de072ccb1d77b1afa
Emmi <30769742+pantoffelemmy@users.noreply.github.com>
2023-03-13 12:44:52 +0100

    grammar

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,7 +9,7 @@
 {/if}
 
 {#if $page.status == 500}
-  <p>Irgendwas ist bei uns schief gelaufen, das hätte nicht passieren sollen.</p>
+  <p>Irgendwas ist bei uns schiefgelaufen, das hätte nicht passieren sollen.</p>
   <p>Sag uns gerne, wie du hier hier gelandet bist, damit wir das Problem beheben können: <a href="https://discord.com/channels/629333468299526164/1021556229857882182" target="_blank" rel="noreferrer">Feedback-Forum auf Discord</a></p>
 {/if}
 
