eb85a7d25c6a1fbc9550f70ba7372faf4d5408cf
ptolemy <30769742+pantoffelemmy@users.noreply.github.com>
2023-03-13 13:58:39 +0100

    Error Handling (#56)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<h1>Fehler {$page.status}: {$page.error.message}</h1>
+
+{#if $page.status == 404}
+  <p>Diese Seite existiert nicht.</p>
+{/if}
+
+{#if $page.status == 500}
+  <p>Irgendwas ist bei uns schiefgelaufen, das hätte nicht passieren sollen.</p>
+  <p>Sag uns gerne, wie du hier hier gelandet bist, damit wir das Problem beheben können: <a href="https://discord.com/channels/629333468299526164/1021556229857882182" target="_blank" rel="noreferrer">Feedback-Forum auf Discord</a></p>
+{/if}
+
+<a href="/">Zurück zur Startseite</a>
