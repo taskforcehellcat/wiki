8ca8ce945a6b2ef1d5cad47422cfbe87aa4f25ba
Emmi <30769742+pantoffelemmy@users.noreply.github.com>
2023-06-01 22:13:20 +0200

    update sveltekit and node modules

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,26 @@
 import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  extensions: ['.svelte', '.svx'],
-  preprocess: [
-    mdsvex({
-      rehypePlugins: [rehypeSlug]
-    }),
-    preprocess()
-  ],
-  // preprocess: preprocess(),
-  kit: {
-    adapter: adapter()
-  },
-  vitePlugin: {
-    experimental: {
-      inspector: true
-    }
-  }
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	extensions: ['.svelte', '.svx'],
+	preprocess: [
+		mdsvex({
+			rehypePlugins: [rehypeSlug]
+		}),
+		vitePreprocess()
+	],
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
