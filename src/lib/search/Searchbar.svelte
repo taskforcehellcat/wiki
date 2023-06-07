9add42853142f4292a578a9590cbb49aa6b5d1b0
Emmi <30769742+pantoffelemmy@users.noreply.github.com>
2023-06-07 02:33:51 +0200

    added keys to search bar

--- src/lib/search/Searchbar.svelte
+++ src/lib/search/Searchbar.svelte
@@ -1,5 +1,6 @@
 <script>
   import SearchModal from '$lib/search/SearchModal.svelte';
+  import Keys from '$lib/wikitools/Keys.svelte';
   import { searchInUse } from './stores';
 </script>
 
@@ -8,7 +9,10 @@
   on:click={() => {
     $searchInUse = true;
   }}
-/>
+>
+  <span class="placeholder">Wiki durchsuchen...</span>
+  <span><Keys keys={'CTRL, K'} /></span>
+</button>
 
 {#if $searchInUse}
   <SearchModal />
@@ -20,26 +24,14 @@
     height: 100%;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     cursor: pointer;
     background-color: #05294d07;
     border: 1px solid var(--border);
     padding-inline: 1.5rem;
     border-radius: 0.6rem;
 
-    &::before {
-      content: 'search';
-      display: inline-block;
-      width: 1.5rem;
-      font-family: 'Material Icons Rounded';
-      font-size: 2.1rem;
-      color: #687076;
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-      margin-inline-end: 1.5rem;
-    }
-
-    &::after {
+    .placeholder {
       content: 'Wiki durchsuchen...';
       display: inline-block;
       color: #687076;
@@ -47,7 +39,22 @@
       background-size: contain;
       background-repeat: no-repeat;
       background-position: center;
-      margin-inline-end: 1rem;
+      margin-inline-end: 1.5rem;
+      white-space: nowrap;
+
+      &::before {
+        content: 'search';
+        display: inline-block;
+        width: 1.5rem;
+        font-family: 'Material Icons Rounded';
+        font-size: 2.1rem;
+        color: #687076;
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        margin-inline-end: 1.5rem;
+        vertical-align: -22%; // i dont want to talk about it
+      }
     }
   }
 </style>
