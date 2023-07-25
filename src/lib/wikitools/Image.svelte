3674ba3c0f0fd04bb4784e92c84737528ce3c020
Venrix <35926556+Venrix@users.noreply.github.com>
2023-07-25 23:27:59 +0200

    added Image component
    should handle both themes

---
+++ src/lib/wikitools/Image.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { themeId } from '$lib/pickers/stores';
+
+  export let lightsrc: string;
+  export let darksrc: string;
+  export let alt: string;
+
+  let prefersDark: boolean = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+</script>
+
+{#if $themeId === 'dark'}
+  <img src={darksrc} {alt} />
+{:else if $themeId === 'light'}
+  <img src={lightsrc} {alt} />
+{:else if $themeId === 'auto'}
+  {#if prefersDark}
+    <img src={darksrc} {alt} />
+  {:else}
+    <img src={lightsrc} {alt} />
+  {/if}
+{/if}
