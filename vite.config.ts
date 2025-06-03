f51f8cd75dcd770c069f25fc37ae97bd0b658257
Venrix <35926556+Venrix@users.noreply.github.com>
2025-06-03 11:02:17 +0200

    upgrade scss api

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
+import dynamicImport from 'vite-plugin-dynamic-import';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import dynamicImport from 'vite-plugin-dynamic-import';
 
-const config = {
-  plugins: [sveltekit(), dynamicImport()]
-};
-
-export default config;
+export default defineConfig({
+  plugins: [sveltekit(), dynamicImport()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  }
+});
